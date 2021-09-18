5f29609e243243e5d9a5b1920929eebb228a155d
Andrea Battaglia <andreabattaglia@users.noreply.github.com>
2021-09-18 12:46:08 +0200

    1.0.0.alpha 7 release

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-echo "Datacenter - Plant Manager"
 ./mvnw clean package -U -Pnative -Dquarkus.native.container-build=true
 docker rmi quay.io/qiotmanufacturing/datacenter-plant-manager:1.0.0-alpha7 --force
 docker build -f src/main/docker/Dockerfile.native -t quay.io/qiotmanufacturing/datacenter-plant-manager:1.0.0-alpha7 .
